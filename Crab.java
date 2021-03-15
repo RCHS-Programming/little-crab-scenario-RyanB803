3209297ee40e027ed6d23205844dee19e3687cd7
RyanB803 <ryanbishop803@gmail.com>
2021-03-15 10:33:55 -0400

    submission1

--- Crab.java
+++ Crab.java
@@ -7,6 +7,27 @@ public class Crab extends Actor
 {
     public void act()
     {
+        if (isAtEdge())
+        {
+          turn(17);   
+        }  
+        
+        
+        if(Greenfoot.getRandomNumber(100)<10)
+        {
+          turn(  Greenfoot.getRandomNumber(91)-45); 
+        }   
+        
+        
+        
+       move(); 
+       
+       if(isTouching(worm.class))
+       {
+         removeTouching(worm.class);   
+        }   
+       
+        turn(5);
         
     }
     
